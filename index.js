09a35723fb42334200d38b9de483eca88003dfea
BigMike783 <45283126+BigMike783@users.noreply.github.com>
2019-07-22 22:09:02 -0600

    added style and content

---
+++ index.js
@@ -0,0 +1,39 @@
+document.querySelector('.contaner').onmousemove = (e) =>{
+    const x = e.pageX - e.target.offsetLeft;
+    const y = e.pageY - e.target.offsetTop;
+
+    e.target.style.setProperty('--x', `${ x }px`)
+    e.target.style.setProperty('--y', `${ y }px`)
+}
+//938 for about
+//1496 for projects
+//3226 for form
+window.addEventListener('scroll', () => {
+    let position = window.scrollY
+    const about = document.querySelector('.aboutcontaner')
+    const jobber = document.querySelector('.jobber')
+    const growth = document.querySelector('.growth')
+    const form = document.querySelector('.formbox')
+    if(position >= 807){
+        about.setAttribute("style", "opacity:1; transform: scale(1) " )
+    }else{
+        about.setAttribute("style", "opacity:0; transform: scale(.5)" )
+    }
+    if(position >= 1496){
+
+        growth.setAttribute("style", "opacity:1; transform: scale(1) " )
+    }else{
+
+        growth.setAttribute("style", "opacity:0; transform: scale(.5)" )
+    }
+    if(position >= 1900){
+        jobber.setAttribute("style", "opacity:1; transform: scale(1) " )
+    }else{
+        jobber.setAttribute("style", "opacity:0; transform: scale(.5)" )
+    }
+    if(position >= 3226){
+        form.setAttribute("style", "opacity:1; transform: scale(1) " )
+    }else{
+        form.setAttribute("style", "opacity:0; transform: scale(.5)" )
+    }
+})
